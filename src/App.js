3aa513f42bbbc839ae3a3969e4a9661c4d7a50c5
Othman <red.fantastic@hotmail.com>
2020-04-21 15:45:53 +0300

    SASS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import HomePage from './pages/homepage/homepage.component';
+// import HomePage from './pages/homepage/homepage.component';
+import HomePage from "./homepage.components";
 
 function App() {
   return (
     <div>
+      {/* <HomePage /> */}
       <HomePage />
     </div>
   );
